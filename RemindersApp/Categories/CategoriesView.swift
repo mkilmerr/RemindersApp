cabe044e87389f61fa468eb03f629d6bed8aac51
mkilmer <marcoskilmer1@gmail.com>
2020-08-29 11:44:38 -0300

    Create Views and Targets

---
+++ RemindersApp/Categories/CategoriesView.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriesView.swift
+//  RemindersApp
+//
+//  Created by Marcos Kilmer on 29/08/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ RemindersApp/Extensions/UIView+AutoLayout.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+AutoLayout.swift
+//  RemindersApp
+//
+//  Created by Marcos Kilmer on 29/08/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+import Foundation
