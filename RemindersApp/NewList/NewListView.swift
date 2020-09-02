5ee76f046bf3fa5858b9e842d97e1b8c111ffd96
mkilmer <marcoskilmer1@gmail.com>
2020-09-02 10:41:29 -0300

    Categorie Images Added on NewListView

--- RemindersApp/NewList/NewListView.swift
+++ RemindersApp/NewList/NewListView.swift
@@ -12,6 +12,8 @@ class NewListView: UIView {
     lazy var contentViewSize = CGSize(width: self.frame.width, height: self.frame.height + 100)
     lazy var colors:[UIColor] = [.red,.gray,.blue,.brown, .green, .purple]
     lazy var secondColor:[UIColor] = [.systemPink,.magenta,.black,.orange,.systemTeal,.tertiarySystemFill]
+    lazy var categorieImageArrayFirst = [UIImage(systemName: "book.fill"), UIImage(systemName: "calendar.circle.fill"), UIImage(systemName: "bookmark.fill"), UIImage(systemName: "umbrella.fill"), UIImage(systemName: "speaker.fill"), UIImage(systemName: "flag.fill")]
+    lazy var categorieImageArraySecond = [UIImage(systemName: "camera.fill"), UIImage(systemName: "hammer.fill"), UIImage(systemName: "printer.fill"), UIImage(systemName: "pin.fill"), UIImage(systemName: "mappin.circle.fill"), UIImage(systemName: "shield.fill")]
     
     lazy var cancelButton: UIButton =  {
         let button = UIButton(type: .system)
@@ -66,7 +68,7 @@ class NewListView: UIView {
         let stack = UIStackView()
         stack.axis = .horizontal
         stack.distribution = .fillEqually
-        stack.spacing = 8
+        stack.spacing = 15
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
@@ -75,7 +77,7 @@ class NewListView: UIView {
         let stack = UIStackView()
         stack.axis = .horizontal
         stack.distribution = .fillEqually
-        stack.spacing = 8
+        stack.spacing = 15
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
         
@@ -89,6 +91,24 @@ class NewListView: UIView {
         return scroll
     }()
     
+    lazy var firstCategorieImageStackView : UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 40
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    lazy var secondCategorieImageStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 35
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemBackground
@@ -99,6 +119,14 @@ class NewListView: UIView {
         }
         self.secondColor.forEach { (color) in
             self.createColorButton(color, stack: self.secondColorStackView)
+        }
+        
+        self.categorieImageArrayFirst.forEach { (image) in
+            self.createCategorieImageButton(image!, stack: firstCategorieImageStackView)
+        }
+        
+        self.categorieImageArraySecond.forEach { (image) in
+            self.createCategorieImageButton(image!, stack: secondCategorieImageStackView)
         }
         
         setupComponents()
@@ -118,12 +146,12 @@ extension NewListView {
         addSubview(headerStackView)
         backgroundCategorieImage.addSubview(categorieImage)
         addSubviewWithAutoLayout(backgroundCategorieImage)
-        //        addSubview(colorStackView)
-        //        addSubview(secondColorStackView)
         addSubview(scrollView)
-        
         scrollView.addSubview(colorStackView)
         scrollView.addSubview(secondColorStackView)
+        scrollView.addSubview(firstCategorieImageStackView)
+        scrollView.addSubview(secondCategorieImageStackView)
+        
         
     }
 }
@@ -134,7 +162,8 @@ extension NewListView {
     func setupConstraints() {
         NSLayoutConstraint.activate([
             headerStackView.topAnchor.constraint(equalTo:topAnchor),
-            headerStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0)
+            headerStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            headerStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
         ])
         
         NSLayoutConstraint.activate([
@@ -165,10 +194,20 @@ extension NewListView {
             scrollView.widthAnchor.constraint(equalTo: self.widthAnchor),
             scrollView.heightAnchor.constraint(equalTo: self.heightAnchor)
         ])
+        
+        NSLayoutConstraint.activate([
+            firstCategorieImageStackView.topAnchor.constraint(equalTo: secondColorStackView.bottomAnchor, constant: 20),
+            firstCategorieImageStackView.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            secondCategorieImageStackView.topAnchor.constraint(equalTo: firstCategorieImageStackView.bottomAnchor, constant: 10),
+            secondCategorieImageStackView.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
     }
 }
 
-//MARK:- COLOR BUTTONS
+//MARK:- COLOR BUTTONS AND CATEGORIE IMAGES
 
 extension NewListView {
     func createColorButton(_ color:UIColor, stack:UIStackView){
@@ -184,7 +223,19 @@ extension NewListView {
         
     }
     
+    func createCategorieImageButton(_ image:UIImage, stack: UIStackView) {
+        let button = UIButton(type: .system)
+        button.setImage(image, for: .normal)
+        button.tintColor = UIColor.label
+        button.addTarget(self, action: #selector(categorieImageTapped(_:)), for: .allEvents)
+        stack.addArrangedSubview(button)
+    }
+    
     @objc func colorButtonTapped(_ sender:UIButton){
         self.backgroundCategorieImage.backgroundColor = sender.backgroundColor
+    }
+    
+    @objc func categorieImageTapped(_ sender:UIButton) {
+        self.categorieImage.image = sender.imageView?.image
     }
 }
